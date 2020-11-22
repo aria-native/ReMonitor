ac10f4966e235c00683b3ce1c4eff5b0314e0929
knothole <knotholex@gmail.com>
2020-11-22 09:40:46 +0900

    Add empty macOS app target

---
+++ Sources/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReMoniter
+//
+//  Created by knothole on 11/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sources/App/ReMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMoniterApp.swift
+//  ReMoniter
+//
+//  Created by knothole on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct ReMoniterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
